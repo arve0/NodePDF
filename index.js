689442c084657391cf264e8b855d9334a5dc5006
arve0 <arve.seljebu@gmail.com>
2017-03-07 21:34:49 +0100

    set default paperSize to 1050x1185

--- index.js
+++ index.js
@@ -4,25 +4,27 @@ var child = require('./child.js');
 
 var defaults = {
   viewportSize: {
-    width: 1024,
-    height: 1448
+    // this should be equal to paper size
+    width: 1050,
+    height: 1485
   },
   paperSize: {
-    width: 1024,
-    height: 1448,
-    format: 'A4',
+    /**
+     * A4 ratio in millimeters: 210 x 297
+     * DPI is hardcoded 72 in phantomJS.
+     * A resolution of 1050px will give 1050 / 72 * 25.4 ~ 370 mm width,
+     * which is way much larger than A4. Most printers will handle this,
+     * and scale correctly to given paper source.
+     */
+    width: 1050,
+    height: 1485,
     orientation: 'portrait',
-    margin: {
-      top: '2.5cm',
-      right: '2.5cm',
-      bottom: '2.5cm',
-      left: '2.5cm'
-    }
+    margin: '1cm'
   },
-  zoomFactor: 1,
   args: '',
   captureDelay: 100
 };
+
 
 /**
  * Render pages til PDFs.
